29074c91276528d296021a9d46c90bc86746b88f
maolei_ubuntu22 <maolei025@qq.com>
2023-06-08 00:53:36 +0800

    add github outh

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,12 @@
 import NextAuth from 'next-auth';
-import {GoogleProvider} from 'next-auth/providers/google';
+import GitHubProvider from "next-auth/providers/github";
 
 const handler = NextAuth({
     providers:[
-        GoogleProvider({
-            clientId: '',
-            clientSecret: '',
-        })
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET
+          })
     ],
     async session({session}){
 
